666fe1893a6e16b70d76cdc7512abf72400d9d38
thamayanthik <thamayanthi.g@gmail.com>
2022-07-09 21:30:06 -0300

    Recursion

--- src/com/formation/algoblitz/recursion/CountElementsInBinaryTree.java
+++ src/com/formation/algoblitz/recursion/CountElementsInBinaryTree.java
@@ -1,4 +1,25 @@
 package com.formation.algoblitz.recursion;
 
+import com.formation.util.TreeNode;
+
 public class CountElementsInBinaryTree {
+    public static void main(String[] args) {
+        TreeNode l2 = new TreeNode(5, null, null);
+        TreeNode r2 = new TreeNode(4, null, null);
+
+        TreeNode l1 = new TreeNode(7, l2, r2);
+        TreeNode r1 = new TreeNode(3, null, null);
+        TreeNode root = new TreeNode(1, l1, r1);
+        int count = countElement(root);
+        System.out.println(count);
+
+    }
+
+    private static int countElement(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return 1 + countElement(root.left) + countElement(root.right);
+        }
+    }
 }
---
+++ src/com/formation/util/TreeNode.java
@@ -0,0 +1,13 @@
+package com.formation.util;
+
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int v, TreeNode l, TreeNode r) {
+        value = v;
+        left = l;
+        right = r;
+    }
+}
